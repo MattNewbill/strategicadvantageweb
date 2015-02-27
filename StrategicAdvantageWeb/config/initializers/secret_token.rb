cb2d8c6f245c97286a86b7f8c83bfe94d3793cf2
Matt Newbill <matt@bitworkssystems.com>
2015-02-26 19:15:27 -0800

    init

---
+++ StrategicAdvantageWeb/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StrategicAdvantageWeb::Application.config.secret_key_base = '8b8510a81b3c7d3ee9e51ac8cae2fd2bc61acdda0465b6335188a8b50ef1ac8d4d17d414173ece8774af85031a3779c6faec1ab3a05369c80829abe101b80706'
